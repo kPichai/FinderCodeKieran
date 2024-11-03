d930e5dff6cbb1c7f69e584f612ebc649ef9e826
kpi <kieran.pichai@menloschool.org>
2024-11-03 12:17:33 -0800

    adasads

--- src/Finder.java
+++ src/Finder.java
@@ -1,5 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  * Finder
  * A puzzle written by Zach Blick
@@ -11,17 +13,51 @@ import java.io.IOException;
 
 public class Finder {
 
+    private static final int p = 100519;
+    private static final int radix = 256;
     private static final String INVALID = "INVALID KEY";
+    private ArrayList<Node>[] hashTable = new ArrayList[p];
 
     public Finder() {}
 
     public void buildTable(BufferedReader br, int keyCol, int valCol) throws IOException {
-        // TODO: Complete the buildTable() function!
+        String line = br.readLine();
+        String[] cur;
+        String key;
+        String val;
+        while (line != null) {
+            cur = line.split(",");
+            key = cur[keyCol];
+            val = cur[valCol];
+            addToHashTable(new Node(key, val));
+            line = br.readLine();
+        }
         br.close();
     }
 
+    public void addToHashTable(Node n) {
+        hashTable[hash(n.getKey())].add(n);
+    }
+
+    public int hash(String str) {
+        return polyRollingHash(str, 0);
+    }
+
+    public int polyRollingHash(String str, int currentHash) {
+        if (str.isEmpty()) {
+            return currentHash;
+        }
+        int next = (((currentHash * radix) % p) + str.charAt(0)) % p;
+        return polyRollingHash(str.substring(1), next);
+    }
+
     public String query(String key){
-        // TODO: Complete the query() function!
+        ArrayList<Node> keyValSet = hashTable[hash(key)];
+        for (Node n : keyValSet) {
+            if (n.getKey().equals(key)) {
+                return n.getValue();
+            }
+        }
         return INVALID;
     }
 }
